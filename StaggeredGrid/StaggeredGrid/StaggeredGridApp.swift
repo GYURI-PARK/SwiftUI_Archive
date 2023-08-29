42f556e789bffedb21246b30e06b91f6612c9cd1
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-08-29 13:56:36 +0900

    add: StaggeredGrid

---
+++ StaggeredGrid/StaggeredGrid/StaggeredGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaggeredGridApp.swift
+//  StaggeredGrid
+//
+//  Created by GYURI PARK on 2023/08/29.
+//
+
+import SwiftUI
+
+@main
+struct StaggeredGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
